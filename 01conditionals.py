a1e28cc0adfc0be526371627c88f56c80fdcb2c2
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2023-01-31 23:47:13 -0500

    conditionals

--- 01conditionals.py
+++ 01conditionals.py
@@ -1,1 +1,10 @@
 # if
+x = int(input("whats x?"))
+y = int(input("whats y?"))
+
+if x < y:
+    print("x is less than y")
+if x > y:
+    print("x is greater than y")
+if x == y:
+    print("x is equal to y")
